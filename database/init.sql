31e1dbcf320b4f6edb3f774fcfe5936d7dd8eef9
Sandlayth <modepadu95@riseup.net>
2017-04-11 08:17:00 +0200

    Database simpified for the first sprint

--- database/init.sql
+++ database/init.sql
@@ -17,7 +17,10 @@ create table menu(
     idMenu integer not null auto_increment,
     name varchar(50) not null,
     description varchar(300),
-    price integer,
+    price float,
+	pizzaNumber integer not null,
+	drinkNumber integer not null,
+	dessertNumber integer not null,
     primary key(idMenu)
 );
 
@@ -27,7 +30,7 @@ create table pizza(
     idPizza integer not null auto_increment,
     name varchar(50) not null,
     description varchar(200),
-    price integer,
+    price float,
     primary key(idPizza)
 );
 
@@ -42,7 +45,7 @@ create table ingredient(
 create table dessert(
     idDessert integer not null auto_increment,
     name varchar(50) not null,
-    price integer, 
+    price float, 
     primary key(idDessert)
 );
 
@@ -50,7 +53,7 @@ create table dessert(
 create table drink(
     idDrink integer not null auto_increment,
     name varchar(50) not null,
-    price integer,
+    price float,
     primary key(idDrink)
 );
 
@@ -63,39 +66,4 @@ create table pizzaIngredient(
 
 alter table pizzaIngredient
     add constraint FK_idIngredient_pizzaIngredient foreign key(idIngredient) references ingredient(idIngredient),
-    add constraint FK_idPizza_pizzaIngredient foreign key(idPizza) references pizza(idPizza);
-
-
-# Pizza in Menu
-create table menuPizza(
-    idPizza integer not null,
-    idMenu integer not null,
-    primary key(idPizza, idMenu)
-);
-
-alter table menuPizza
-    add constraint FK_idMenu_menuPizza foreign key(idMenu) references menu(idMenu),
-    add constraint FK_idPizza_menuPizza foreign key(idPizza) references pizza(idPizza);
-
-# Dessert in Menu
-create table menuDessert(
-    idDessert integer not null,
-    idMenu integer not null,
-    primary key(idMenu, idDessert)
-);
-
-alter table menuDessert
-    add constraint FK_idMenu_menuDessert foreign key(idMenu) references menu(idMenu),
-    add constraint FK_idDessert_menuDessert foreign key(idDessert) references dessert(idDessert);
-
-# Drink in Menu
-create table menuDrink(
-    idDrink integer not null,
-    idMenu integer not null,
-    primary key(idMenu, idDrink)
-);
-
-alter table menuDrink
-    add constraint FK_idMenu_menuDrink foreign key(idMenu) references menu(idMenu),
-    add constraint FK_idDessert_menuDrink foreign key(idDrink) references drink(idDrink);
-
+    add constraint FK_idPizza_pizzaIngredient foreign key(idPizza) references pizza(idPizza)
